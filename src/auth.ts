ad0da5637a3baa1fae4cb7fc01158c74308549e9
chrislkl <christopher.lew916@gmail.com>
2025-05-05 11:07:20 -0400

    removing comments

--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,9 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// import directly from @clerk/express — no createClerkExpress here
 import { clerkMiddleware, requireAuth, clerkClient, getAuth } from "@clerk/express";
 
 export { clerkMiddleware, requireAuth, clerkClient, getAuth };
 
-// then your Prisma client in the same file or a separate one
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
